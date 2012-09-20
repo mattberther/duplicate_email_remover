51a8e6895ed1840f32a26ff49261d999fe5597ce
Matt Berther <matt@mattberther.com>
2012-09-20 11:25:31 -0600

    iterate messages for mail item processor

--- Program.cs
+++ Program.cs
@@ -1,14 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Microsoft.Office.Interop.Outlook;
 
-namespace Outlook_Duplicate_Email_Remover
+namespace com.mattberther.deduper
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var mailItemProcessor = new MailItemProcessor(true);
+            mailItemProcessor.RegisterKeyGenerator(new MessageIdKeyGenerator());
+            mailItemProcessor.RegisterKeyGenerator(new MessageAttributeKeyGenerator());
+
+            var outlook = new Application();
+            var outlookNamespace = outlook.GetNamespace("MAPI");
+            var folder = outlookNamespace.Folders[1].Folders["Archive"];
+
+            foreach (var item in folder.Items)
+            {
+                var mailItem = item as MailItem;
+                if (mailItem != null)
+                {
+                    mailItemProcessor.ProcessMailItem(mailItem);    
+                }
+            }
+
+            Console.WriteLine("Deleted items: {0}", mailItemProcessor.ItemsDeleted);
+            Console.WriteLine("Total items: {0}", folder.Items.Count);
+            Console.WriteLine("** PRESS ENTER **");
+            Console.ReadLine();
         }
     }
 }
